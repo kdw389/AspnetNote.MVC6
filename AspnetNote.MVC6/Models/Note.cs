5707841d8dbfd347f6cba48db1adff56ea1e8c7e
kdw <dw.kim@iljin.co.kr>
2019-05-22 16:32:51 +0900

    초기 커밋.

---
+++ AspnetNote.MVC6/Models/Note.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AspnetNote.MVC6.Models
+{
+    public class Note
+    {
+        /// <summary>
+        /// 게시물 No(PK)
+        /// </summary>
+        [Key]
+        public int NoteNo { get; set; }
+
+        /// <summary>
+        /// 게시물 제목
+        /// </summary>
+        [Required]  // Not Null
+        public string NoteTitle { get; set; }
+
+        /// <summary>
+        /// 게시물 내용
+        /// </summary>
+        [Required]  // Not Null
+        public string NoteContents { get; set; }
+
+        /// <summary>
+        /// 작성자 번호
+        /// </summary>
+        [Required]  // Not NUll
+        public int UseerNo { get; set; }
+
+
+        [ForeignKey("UserNo")]  //Join을 위한 ForeignKey 설정
+        public virtual User User { get; set; }
+    }
+}
---
+++ AspnetNote.MVC6/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspnetNote.MVC6.Models
+{
+    public class User
+    {
+        /// <summary>
+        /// 사용자 번호(PK)
+        /// </summary>
+        [Key]   //PK 설정
+        public int UserNo { get; set; }
+
+        /// <summary>
+        /// 사용자 이름
+        /// </summary>
+        [Required]  //Not Null 설정
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// 사용자ID
+        /// </summary>
+        [Required]  //Not Null 설정
+        public string UserId { get; set; }
+
+        /// <summary>
+        /// 사용자 Password
+        /// </summary>
+        [Required]  //Not Null 설정
+        public string UserPassword { get; set; }
+    }
+}
