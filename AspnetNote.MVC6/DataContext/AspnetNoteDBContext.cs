fc2564057be5359416f9fe2ba60e926fed029057
kdw <dw.kim@iljin.co.kr>
2019-05-22 17:27:01 +0900

    DataBase 만들기

---
+++ AspnetNote.MVC6/DataContext/AspnetNoteDBContext.cs
@@ -0,0 +1,22 @@
+﻿using AspnetNote.MVC6.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetNote.MVC6.DataContext
+{
+    public class AspnetNoteDBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+
+        public DbSet<Note> Notes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=197.200.1.63,1433;Initial Catalog=AspnetNoteDb;Persist Security Info=True;User ID=kdw;Password=kdw; ");
+        }
+    }
+}
